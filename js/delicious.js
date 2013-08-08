242856d3e437172548814ebaa3d2e74981cd50c4
bradcedergren <b_cedergren@hotmail.com>
2013-08-08 12:59:06 -0500

    Initial Commit

---
+++ js/delicious.js
@@ -0,0 +1,26 @@
+$(function() {
+	$('.mobilemenu-toggle').click(function() {
+		$('#NavSlide').slideToggle(200);
+	});
+	
+	$('.add.favorites').click(function() {
+		$(this).toggleClass('on off')
+		if ($(this).hasClass('off')) {
+			$(this).attr('title', 'add to favorites')
+		} else {
+			$(this).attr('title', 'remove from favorites')
+		}
+	});
+	
+	$("#ShowSiteBar").click(function() {
+		$("#siteBar").fadeToggle(400);
+	})
+	
+	setTimeout(function(){
+    	$('#siteBar').fadeToggle(400)
+    },3000)
+	
+	setTimeout(function(){
+    	$('#doughBoy').fadeToggle(400)
+    },5000)
+});
